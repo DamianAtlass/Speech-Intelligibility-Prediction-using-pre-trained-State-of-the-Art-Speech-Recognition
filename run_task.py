4edfecf378d8add9990b0eb550167f69125bad00
DamianAtlass <damianatlass@gmx.de>
2026-04-21 12:17:43 +0200

    fix

--- run_task.py
+++ run_task.py
@@ -1,19 +1,33 @@
+print("imports start")
+
 import sys
-from utils.config_dataclasses import get_config
+from utils.config_dataclasses import get_config, TrainingConfig
 from pathlib import Path
 from utils.grid_utils import get_grid, apply_split
-
+print("----")
+from train_whisper import train_whisper
+print("----")
+from shutil import copyfile
+print("imports done")
 
 def main():
     if sys.version_info[0] < 3 and sys.version_info[1] < 12:
         raise Exception("Must be using Python 3.12 or later!")
 
-if __name__ == '__main__':
-    main()
-    config = get_config("tmp_config.ini")
+    config_file_path = "tmp_config.ini"
+    config = get_config(config_file_path)
 
-    cwd = Path.cwd()
+    Path.mkdir(config.output_dir.parent, exist_ok=True)
+    Path.mkdir(config.output_dir)
+    copyfile(Path.cwd() / config_file_path, config.output_dir / config_file_path)
+
     dataset = get_grid()
     dataset = apply_split(dataset, config)
 
+    if isinstance(config, TrainingConfig):
+        train_whisper(config, dataset)
+
     print()
+
+if __name__ == '__main__':
+    main()
