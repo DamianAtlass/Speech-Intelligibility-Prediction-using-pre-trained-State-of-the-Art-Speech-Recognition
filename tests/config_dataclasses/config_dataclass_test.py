daba38c47d0c01e8708c899ff953a51717a6754b
DamianAtlass <damianatlass@gmx.de>
2026-05-10 00:41:41 +0200

    add tests

---
+++ tests/config_dataclasses/config_dataclass_test.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import os
+import pytest
+
+from utils.config_dataclasses import get_config
+
+TEST_FOLDER = Path.cwd()/"tests/config_dataclasses"
+
+@pytest.mark.parametrize("file_name", [
+        "test_inference_config.ini",
+        "test_group_inference_config.ini",
+        "test_training_config.ini",
+#        "test_group_training_config.ini",
+])
+def test_loading_and_saving(file_name):
+    file_path = TEST_FOLDER/file_name
+    tmp_file = TEST_FOLDER/"tmp.ini"
+
+    if tmp_file.exists():
+        os.remove(tmp_file)
+
+    config = get_config(file_path)
+
+    config.save_to_file(tmp_file)
+
+    new_config = get_config(tmp_file)
+
+    if tmp_file.exists():
+        os.remove(tmp_file)
+
