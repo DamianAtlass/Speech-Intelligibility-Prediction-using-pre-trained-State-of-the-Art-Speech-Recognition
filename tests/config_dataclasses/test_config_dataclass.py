2c8b6f607c7db1ca777c96a9e488f6a68a31f524
DamianAtlass <damianatlass@gmx.de>
2026-05-21 00:18:16 +0200

    update configs

--- tests/config_dataclasses/test_config_dataclass.py
+++ tests/config_dataclasses/test_config_dataclass.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import os
 import pytest
 
-from utils.config_dataclasses import get_config, unfold_config
+from utils.config_dataclasses import get_config, unfold_config, to_int_or_float
 
 TEST_FOLDER = Path.cwd()/"tests/config_dataclasses"
 
@@ -10,9 +10,11 @@ TEST_FOLDER = Path.cwd()/"tests/config_dataclasses"
         "test_inference_config.ini",
         "test_group_inference_config.ini",
         "test_training_config.ini",
-#        "test_group_training_config.ini",
+        "test_training_config2.ini",
 ])
 def test_loading_and_saving(file_name):
+    if file_name =="test_training_config1.ini":
+        pass
     file_path = TEST_FOLDER/file_name
     tmp_file = TEST_FOLDER/"tmp.ini"
 
@@ -30,16 +32,33 @@ def test_loading_and_saving(file_name):
     if tmp_file.exists():
         os.remove(tmp_file)
 
-@pytest.mark.parametrize(("file_name", "l"), [
+
+def test_loading_bad_file(file_name = "test_training_config_bad.ini"):
+
+    file_path = TEST_FOLDER/file_name
+    try:
+        get_config(file_path)
+    except ValueError:
+        assert True
+
+
+@pytest.mark.parametrize(("file_name", "length"), [
         ("test_inference_config.ini", 1),
         ("test_group_inference_config.ini", 3),
         ("test_training_config.ini", 1),
 #        "test_group_training_config.ini",
 ])
-def test_unfold_config(file_name, l):
+def test_unfold_config(file_name, length):
     file_path = TEST_FOLDER / file_name
     config = get_config(file_path)
 
     configs = unfold_config(config)
 
-    assert len(configs) == l
+    assert len(configs) == length
+
+@pytest.mark.parametrize(["num_string", "type_"],
+                         [("6", int),
+                          ("2.1", float)])
+def test_to_int_or_float(num_string, type_):
+    num = to_int_or_float(num_string)
+    assert isinstance(num, type_)
