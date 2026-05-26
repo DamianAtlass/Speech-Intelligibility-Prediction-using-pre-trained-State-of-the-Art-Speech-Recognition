8ef40e1f1fc3746a6cef89ff5ea927e38f0d0b23
DamianAtlass <damianatlass@gmx.de>
2026-05-26 21:26:56 +0200

    add training test

---
+++ tests/test_training.py
@@ -0,0 +1,48 @@
+import pytest
+
+from dotenv import load_dotenv
+load_dotenv() # needs to be before 'import torch'!
+import torch
+
+from pathlib import Path
+import shutil
+from utils.config_dataclasses import TrainingConfig
+from utils.cuda_utils import select_device
+from utils.grid_utils import get_grid, apply_split
+from train_whisper import train_whisper
+
+
+
+
+
+@pytest.mark.parametrize("perform_training", [False,True])
+
+def test_whisper_training(perform_training):
+
+    config = TrainingConfig(
+        model="whisper",
+        model_type="tiny",
+        model_path=None,
+        output_path=Path("tests/training_test"),
+        dataset_path=Path("datasets/grid/"),
+        train_split=30,
+        test_split=1,
+        val_split=1,
+        perform_training=perform_training,
+        learning_rate=1e-5,
+        num_train_epochs=1,
+        warmup_steps = 5
+    )
+
+    if config.output_path.exists():
+        shutil.rmtree(config.output_path)
+
+    dataset = get_grid(config.dataset_path)
+    dataset = apply_split(dataset, config)
+    config.output_path.mkdir(exist_ok=config.debug)
+    device = select_device()
+
+    train_whisper(config, dataset, device)
+
+    if not (config.output_path/"model.safetensors").is_file():
+        assert False
