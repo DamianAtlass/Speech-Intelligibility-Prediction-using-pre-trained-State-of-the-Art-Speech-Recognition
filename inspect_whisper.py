af2ce4a046d92f50ca20c0e03ceb13a143e01d6f
damian <damianatlass@gmx.de>
2026-06-30 01:47:47 +0200

    stuff

--- inspect_whisper.py
+++ inspect_whisper.py
@@ -1,14 +1,21 @@
 import whisper
 import numpy as np
+
+from utils.wer_needleman_wunsch import wer_needleman_wunsch
 from utils.grid_utils import get_grid
+from utils.grid_bc_utils import get_grid_bc
+from utils.cuda_utils import select_device
 from tqdm import tqdm
+
+from dotenv import load_dotenv
+load_dotenv() # needs to be before 'import torch' to control what gpu to use (since some libs chose automatically)!
 import torch
+
+from utils.grid_utils import convert_short_name_to_ref
 
 
 downloaded_grid_file_25kHz = "datasets/grid/downloaded_grid_files/audio/s23/s23/sraf4p.wav"
 def foo():
-
-    from tqdm import tqdm
 
     model = whisper.load_model("tiny")
     dataset = get_grid()
@@ -16,7 +23,6 @@ def foo():
 
 
     audio = whisper.load_audio(downloaded_grid_file_25kHz)
-    #audio = np.pad(audio, (0, 160_000 - len(audio)))
 
     other_audio = whisper.load_audio(downloaded_grid_file_25kHz)
     import soundfile as sf
@@ -30,69 +36,88 @@ def foo():
     result = whisper.transcribe(model, audio2, condition_on_previous_text=False, word_timestamps=True, beam_size=5, temperature=0)
 
 def compare_transcripts():
-    model = whisper.load_model("tiny")
-    dataset = get_grid()
+    model = whisper.load_model("small")
+    dataset = get_grid_bc()
+    refs = []
+    array_list = []
+    loaded_list = []
 
-    for i, sample in tqdm(enumerate(dataset.select(range(100)))):
-        audio1 = sample["audio"]["array"]
+    for i, sample in tqdm(enumerate(dataset.select(range(100))), total=100):
+        ref = sample["sentence"]
+        audio_array = sample["audio"]["array"]
 
         # load audio and pad/trim it to fit 30 seconds
-        audio2 = whisper.load_audio(sample["audio_path"])
+        audio_loaded = whisper.load_audio(sample["audio_path"], )
 
-        corr = np.correlate(audio1, audio2, mode="full")
-        lag = np.argmax(corr) - (len(audio2) - 1)
+        corr = np.correlate(audio_array, audio_loaded, mode="full")
+        lag = np.argmax(corr) - (len(audio_loaded) - 1)
         if lag != 0:
             print(f"audio not correlating for {i =}")
 
-        audio1 = whisper.pad_or_trim(audio1)
-        audio2 = whisper.pad_or_trim(audio2)
+        audio_array = whisper.pad_or_trim(audio_array)
+        audio_loaded = whisper.pad_or_trim(audio_loaded)
 
-        result1 = whisper.transcribe(model, audio1, condition_on_previous_text=False, word_timestamps=False,
+        result_array = whisper.transcribe(model, audio_array, condition_on_previous_text=False, word_timestamps=False,
                                      beam_size=5, temperature=0)
 
-        result2 = whisper.transcribe(model, audio2, condition_on_previous_text=False, word_timestamps=False,
+        result_loaded = whisper.transcribe(model, audio_loaded, condition_on_previous_text=False, word_timestamps=False,
                                      beam_size=5, temperature=0)
 
-        if result1["text"] != result2["text"]:
-            print(f"text unequal for {i =}")
+        if result_array["text"] != result_loaded["text"]:
+            refs.append(ref)
+            array_list.append(result_array["text"])
+            loaded_list.append(result_loaded["text"])
+            print(f"text unequal for {i=} ({result_array["text"]}  =/= {result_loaded["text"]} )")
+
+    print(f"array: {wer_needleman_wunsch(reference=refs, transcript=array_list)}")
+    print(f"loaded: {wer_needleman_wunsch(reference=refs, transcript=loaded_list)}")
 
     # mean arr: 5.413363646233904e-10
     # min arr: -1.52587890625e-05
     # mean arr: 1.52587890625e-05
 
-def foo():
-    print(whisper.available_models())
-    #for n in ["tiny", "large-v1", "large-v2", "large-v3", "large", "large-v3-turbo", "turbo"]:
-    model1 = whisper.load_model("large")
-    model2 = whisper.load_model("large-v3")
+def find_weird_file():
+    dataset = get_grid()
+    device = select_device()
+    model = whisper.load_model("tiny", device=device)
 
-    same = True
 
-    for k in model1.state_dict():
-        if not torch.equal(model1.state_dict()[k], model1.state_dict()[k]):
-            same = False
-            print("Different at:", k)
+    for sample in tqdm(dataset):
+        audio = sample["audio"]["array"]
+
+
+
+        result = whisper.transcribe(model, audio, condition_on_previous_text=False, word_timestamps=False,
+                                     beam_size=5, temperature=0)
+
+        if len(result["text"]) > 50:
+            print(sample["audio_path"])
+            print(result["text"])
             break
 
-    print("Same weights:", same)
+def inspect_file():
+    file_path = "datasets/grid/downloaded_grid_files/audio/s27/s27/lbihzn.wav"
+    device = select_device()
+    model = whisper.load_model("tiny", device=device)
 
-def find_weird_file():
-    model = whisper.load_model("tiny")
-    dataset = get_grid()
-
-    sample = dataset[2396]
-    audio = sample["audio"]["array"]
-
+    audio = whisper.load_audio(file_path)
     audio = whisper.pad_or_trim(audio)
 
-    result = whisper.transcribe(model, audio, condition_on_previous_text=False, word_timestamps=False,
-                                 beam_size=5, temperature=0)
+    print("reference: ", convert_short_name_to_ref("lbihzn", False))
+    result = whisper.transcribe(model, audio,
+                                condition_on_previous_text=False,
+                                fp16=False,
+                                word_timestamps=True,
+                                beam_size=5,
+                                temperature=0.2,
+                                language="en",
+                                )
 
-
-    print(result["text"])
-
+    print(result)
+    pass
 
 
 
 if __name__ == '__main__':
-    find_weird_file()
+    compare_transcripts()
+
