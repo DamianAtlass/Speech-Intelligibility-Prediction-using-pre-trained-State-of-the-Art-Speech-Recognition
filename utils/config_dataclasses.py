0d4c22844aad26f634d803df131368d470d7726f
DamianAtlass <damianatlass@gmx.de>
2026-07-31 00:43:07 +0200

    WIP

--- utils/config_dataclasses.py
+++ utils/config_dataclasses.py
@@ -90,6 +90,7 @@ class TrainingConfig(Config):
     learning_rate: float
     num_train_epochs: int
     batch_size: int = 16
+    save_and_eval_steps: int = 100
     warmup_steps: int
 
     def __post_init__(self):
@@ -97,6 +98,7 @@ class TrainingConfig(Config):
         self.learning_rate = float(self.learning_rate)
         self.num_train_epochs = int(self.num_train_epochs)
         self.batch_size = int(self.batch_size)
+        self.save_and_eval_steps = int(self.save_and_eval_steps)
         self.warmup_steps = int(self.warmup_steps)
         if isinstance(self.perform_training, str):
             self.perform_training = self.perform_training=="True"
@@ -109,6 +111,7 @@ class TrainingConfig(Config):
             'perform_training',
             'learning_rate',
             'batch_size',
+            'save_and_eval_steps',
             'num_train_epochs',
             'warmup_steps',
         ]
