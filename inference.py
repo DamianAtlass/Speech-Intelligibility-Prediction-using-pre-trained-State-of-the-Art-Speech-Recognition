2d3c5621bad36a96be5c4aae47cafe9dea4b267a
damian <damianatlass@gmx.de>
2026-08-31 20:13:04 +0200

    separate loop and actual inference

--- inference.py
+++ inference.py
@@ -80,7 +80,7 @@ def save_result(sample_dict: dict,
 
     if seperator: logger.info("--------------------")
 
-def inference_whisper(config: InferenceConfig, model: Any, dataset: Dataset, device: torch.device) -> None:
+def inference_loop_whisper(config: InferenceConfig, model: Any, dataset: Dataset, device: torch.device) -> None:
     """
     Perform inference on a batch of data to collect the results and certain internal values. Saves it locally file by file.
     config: InferenceConfig
@@ -90,9 +90,6 @@ def inference_whisper(config: InferenceConfig, model: Any, dataset: Dataset, dev
 
     Returns: None
     """
-    is_list = {
-        "temp": isinstance(config.temperature, list)
-    }
     if config.temperature!=0 and config.beam_size!=1:
         raise ValueError("Beamsize would be overridden for temperature !=1!")
 
@@ -106,52 +103,7 @@ def inference_whisper(config: InferenceConfig, model: Any, dataset: Dataset, dev
                     logger.info(f"Transcribe {sample["audio_path"] if "audio_path" in sample.keys() else sample["id"]} (#{idx_sample}) "
                                 f"{f"(run: {run})" if config.runs_per_sample > 1 else ""} ...")
                     try:
-                        audio_array = torch.tensor(sample["audio"]["array"]).to(device)
-                        audio_array = sip_whisper.pad_or_trim(audio_array)
-
-                        options = {
-                            "model": model,
-                            "audio": audio_array,
-                            "fp16": False,
-                            "beam_size": config.beam_size,
-                            "temperature": config.temperature,
-                            "extract_logprobs": config.extract_logprobs,
-                            "word_timestamps": config.word_timestamps,
-                            "condition_on_previous_text": False,
-                            "language": "en",
-                            "break_after_first_segment": False if config.model.path is None else True, # only with trained models
-                            "subword_timestamps": config.subword_timestamps,
-                        }
-
-                        # results will not be equal (therefore not deterministic) if temperature=!0.0, bc of stochastic sampling
-                        #  https://github.com/openai/whisper/discussions/81
-                        result: dict = sip_whisper.transcribe(**options)
-
-                        file_name = create_filename(
-                            dataset_type=config.data.val_split.dataset_type,
-                            sample=sample, run=run if config.runs_per_sample>1 else None)
-
-                        if config.extract_logprobs:
-                            extracted_logprobs = result.pop("extracted_logprobs")
-                            tokens = [x for s in result["segments"] for x in s["tokens"]]
-                            assert len(tokens) == (0 if extracted_logprobs is None else extracted_logprobs.shape[0]), "Missmatch of logprob and token length"
-
-                            if extracted_logprobs is not None:
-                                logprob_file_path = config.output_path/"logprobs"/f"{file_name}.pt"
-                                if logprob_file_path.exists(): raise FileExistsError
-                                logger.info(f"Save logprobs to {logprob_file_path}")
-                                torch.save(extracted_logprobs, logprob_file_path)
-                                result["logprobs_path"] = str(logprob_file_path.relative_to(Path.cwd()))
-
-                            else:
-                                result["logprobs_path"] = ""
-                                logger.info("No logprobs to save.")
-
-                        if is_list["temp"]:
-                            sample.update({"varrying_transcription_options": {"run": run} })
-
-                        result_data_file_path = config.output_path / "data" / f"{file_name}.json"
-                        save_result(sample.copy(), result, result_data_file_path, counter, True)
+                        inference_whisper(model, config, sample, device, run, counter)
                     except Exception as e:
                         logger.critical(e)
                         exception_log.append(counter)
@@ -164,6 +116,61 @@ def inference_whisper(config: InferenceConfig, model: Any, dataset: Dataset, dev
         logger.critical(f"{len(exception_log)} exceptions occured! ({round(len(exception_log)/num_total_transciptions, 3)*100}%)")
         logger.critical(f"Bad transcriptions at counter= {exception_log}")
 
+def inference_whisper(
+        model,
+        config: InferenceConfig,
+        sample: dict,
+        device: torch.device,
+        run: int,
+        counter: int,):
+
+    audio_array = torch.tensor(sample["audio"]["array"]).to(device)
+    audio_array = sip_whisper.pad_or_trim(audio_array)
+
+    options = {
+        "model": model,
+        "audio": audio_array,
+        "fp16": False,
+        "beam_size": config.beam_size,
+        "temperature": config.temperature,
+        "extract_logprobs": config.extract_logprobs,
+        "word_timestamps": config.word_timestamps,
+        "condition_on_previous_text": False,
+        "language": "en",
+        "break_after_first_segment": False if config.model.path is None else True,  # only with trained models
+        "subword_timestamps": config.subword_timestamps,
+    }
+
+    # results will not be equal (therefore not deterministic) if temperature=!0.0, bc of stochastic sampling
+    #  https://github.com/openai/whisper/discussions/81
+    result: dict = sip_whisper.transcribe(**options)
+
+    file_name = create_filename(
+        dataset_type=config.data.val_split.dataset_type,
+        sample=sample, run=run if config.runs_per_sample > 1 else None)
+
+    if config.extract_logprobs:
+        extracted_logprobs = result.pop("extracted_logprobs")
+        tokens = [x for s in result["segments"] for x in s["tokens"]]
+        assert len(tokens) == (
+            0 if extracted_logprobs is None else extracted_logprobs.shape[0]), "Missmatch of logprob and token length"
+
+        if extracted_logprobs is not None:
+            logprob_file_path = config.output_path / "logprobs" / f"{file_name}.pt"
+            if logprob_file_path.exists(): raise FileExistsError
+            logger.info(f"Save logprobs to {logprob_file_path}")
+            torch.save(extracted_logprobs, logprob_file_path)
+            result["logprobs_path"] = str(logprob_file_path.relative_to(Path.cwd()))
+
+        else:
+            result["logprobs_path"] = ""
+            logger.info("No logprobs to save.")
+
+    if config.runs_per_sample > 1:
+        sample.update({"varrying_transcription_options": {"run": run}})
+
+    result_data_file_path = config.output_path / "data" / f"{file_name}.json"
+    save_result(sample.copy(), result, result_data_file_path, counter, True)
 
 def inference_parekeet(config: InferenceConfig, model: EncDecCTCModelBPE, dataset: Dataset, device: torch.device) -> None:
     with torch.inference_mode():
@@ -224,7 +231,7 @@ def inference(config: InferenceConfig, dataset: DatasetDict, device: torch.devic
 
     #dataset["val"] = dataset["val"].filter(lambda sample: sample["audio_path"]=='datasets/GridIntelligibilityDatabase/BC2007wavs/BC2007/m12/6/s5_bbar7s.wav')
     if config.model.name =="whisper":
-        inference_whisper(config, model, dataset["val"], device)
+        inference_loop_whisper(config, model, dataset["val"], device)
     elif config.model.name =="parakeet":
         inference_parekeet(config, model, dataset["val"], device)
     else:
